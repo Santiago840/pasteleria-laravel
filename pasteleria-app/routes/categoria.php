d30667c6e8f3d90ea603847b1947cff4f31b145a
Santiago840 <santyemma32@gmail.com>
2025-02-07 19:56:39 -0600

    category store route added

--- pasteleria-app/routes/categoria.php
+++ pasteleria-app/routes/categoria.php
@@ -8,8 +8,15 @@ Route::get('/categoria', function () {
     return Inertia::render('Categoria/Categoria');
 })->middleware(['verified', 'auth'])->name('categoria');
 
+Route::get('/agregarCategoria', function(){
+    return Inertia::render('Categoria/AgregarCategoria');
+})->middleware(['auth', 'verified'])->name('agregarCategoria');
+
+Route::get('/editarCategoria', function(){
+    return Inertia::render('Categoria/EditarCategoria');
+})->middleware(['auth', 'verified'])->name('editarCategoria');
+
 Route::resource('/categorias', CategoriaController::class)
     ->only(['index', 'store', 'update', 'destroy'])
     ->middleware(['verified', 'auth']);
 
-    
