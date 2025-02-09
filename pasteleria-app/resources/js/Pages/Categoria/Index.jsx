71b7b2c2885835574f9bb5cbb911d0e4762d01d2
Santiago840 <santyemma32@gmail.com>
2025-02-08 21:32:48 -0600

    Categoria fixed

---
+++ pasteleria-app/resources/js/Pages/Categoria/Index.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PrimaryButton from "@/Components/PrimaryButton";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import AgregarCategoria from "./AgregarCategoria";
+import { Head } from "@inertiajs/react";
+import { Link } from "@inertiajs/react";
+
+export default function Index() {
+    return (
+        <AuthenticatedLayout
+            header={
+                <div>
+                    <h2>Categorías</h2>
+                    <PrimaryButton>
+                        Agregar
+                    </PrimaryButton>
+                </div>
+            }
+        >
+            <Head title="Categoría"></Head>
+            <div>
+                <AgregarCategoria></AgregarCategoria>
+            </div>
+        </AuthenticatedLayout>
+    );
+}
